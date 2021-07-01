92ddb0fa065848a907241190c33ca259b1f16cca
rekkawarShubham <rekkawars111@gmail.com>
2021-07-01 11:15:07 +0530

    --added : Mongo url

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
  const mongoose = require('mongoose')
- mongoose.connect('mongodb://localhost:27017/EmployeeDB',{useNewUrlParser :true},(err)=>{
+ mongoose.connect('mongodb+srv://shubham:$hubhzz123@cluster0.2k8c4.mongodb.net/EmployeeDB',{useNewUrlParser :true},(err)=>{
      if(!err) {console.log("Connection Successfull")}
      else {console.log("Connection unsuccessfull" + err)}
  })
