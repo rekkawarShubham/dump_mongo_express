62e79a20205dabf794dc91f4f51ea464c6c105af
rekkawarShubham <rekkawars111@gmail.com>
2021-07-01 12:46:20 +0530

    Minor changes mongo url

--- model/db.js
+++ model/db.js
@@ -1,7 +1,13 @@
+const { MongoClient } = require('mongodb');
  const mongoose = require('mongoose')
  mongoose.connect('mongodb+srv://shubham:$hubhzz123@cluster0.2k8c4.mongodb.net/EmployeeDB',{useNewUrlParser :true},(err)=>{
      if(!err) {console.log("Connection Successfull")}
      else {console.log("Connection unsuccessfull" + err)}
  })
-
- require('./employee.model')
+// const uri = "mongodb+srv://shubham:$hubhzz123@cluster0.2k8c4.mongodb.net/EmployeeDB?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     if (!err) { console.log("Connection Successfull") }
+//     else { console.log("Connection unsuccessfull" + err) }
+// });
+require('./employee.model')
