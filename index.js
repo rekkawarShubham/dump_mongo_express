85040f21852a1c2fb8be1b64ef7fc633b0a4d87c
rekkawarShubham <rekkawars111@gmail.com>
2021-07-01 11:37:47 +0530

    Minor changes

---
+++ index.js
@@ -0,0 +1,25 @@
+require('./model/db');  
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
+var app = express();    
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
+app.use(bodyparser.json())
+app.set("views",path.join(__dirname,"/views/"));
+app.engine('hbs', exphbs({ extname : 'hbs', defaultLayout : 'mainLayout', layoutsDir : __dirname + '/views/layouts' , handlebars : allowInsecurePrototypeAccess(Handlebars)}));
+app.set('view engine','hbs');
+
+var empplyeeController = require("./controller/employeeController")
+
+app.listen(3000,()=>{
+    console.log("Server has started successfully on port 3000");
+})
+
+app.use("/employee",empplyeeController)
